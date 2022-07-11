52e170a9804cc230d3d331580e15eb50649f3285
Inseok Seo <inseok.seo@sap.com>
2022-07-11 18:21:46 +0900

    Modify chan message

--- url_checker_with_goroutine/main.go
+++ url_checker_with_goroutine/main.go
@@ -17,15 +17,14 @@ func main() {
 	results := make(map[string]string)
 	c := make(chan requestResult)
 	urls := []string{
-		"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
-		"https://www.reddit.com/",
+		"https://www.airbnb.com/",
 		"https://www.google.com/",
-		"https://soundcloud.com/",
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+		"https://www.reddit.com/",
+		"https://www.naver.com/",
 	}
 	for _, url := range urls {
 		go hitURL(url, c)
@@ -41,9 +40,9 @@ func main() {
 
 func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
-	status := "OK"
+	status := "SUCCESS"
 	if err != nil || resp.StatusCode >= 400 {
-		status = "FAILED"
+		status = "FAILURE"
 	}
 	c <- requestResult{url: url, status: status}
 }
