44ad40f655ade945f91fed0492e840c88ba6764d
Inseok Seo <inseok.seo@sap.com>
2022-07-11 14:46:05 +0900

    Initial commit

---
+++ url_checker_with_goroutine/go.mod
@@ -0,0 +1,3 @@
+module github.com/dev-a-z/learngo/url_checker_with_goroutine
+
+go 1.17
---
+++ url_checker_with_goroutine/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("URL Checker with Go Routine!")
+}
